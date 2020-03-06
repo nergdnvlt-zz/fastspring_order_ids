976402763ef75b2ae11117fc56d22cc7a56e729d
Tyler Lundgren <tylerlundgren@yahoo.com>
2020-03-06 16:04:23 -0700

    Collecting all order ID's and holding in memory.

---
+++ fastspring_request.rb
@@ -0,0 +1,57 @@
+require 'httparty'
+require 'Pry'
+
+class FastSpringRequest
+  def self.run(username, password)
+    new(username, password).run
+  end
+
+  def run
+    populate_orders
+
+    doublecheck_orders
+
+    binding.pry
+  end
+
+  def initialize(username, password)
+    @username = username
+    @password = password
+    initial_request ||= set_baseline
+    @total_orders = initial_request["total"]
+    @limit = 500
+    @pages_arr = Array(1..(@total_orders.to_f / @limit).ceil)
+    @orders = []
+  end
+
+  private
+
+  def set_baseline
+    fastspring_request
+  end
+
+  def populate_orders
+    @pages_arr.each do |page|
+      @orders = @orders | fastspring_request(@limit, page)["orders"]
+    end
+  end
+
+  def fastspring_request(limit = 50, page = 1)
+    conn({username: @username, password: @password }, limit, page)
+  end
+
+  def conn(auth, limit, page)
+    HTTParty.get(
+      "https://api.fastspring.com/orders?limit=#{limit}&page=#{page}",
+      basic_auth: auth
+    )
+  end
+
+  def doublecheck_orders
+    if @orders.count != @total_orders
+      populate_orders
+      @order.uniq
+      doublecheck_orders
+    end
+  end
+end
---
+++ runner.rb
@@ -0,0 +1,6 @@
+require './fastspring_request.rb'
+
+fs_username = 'N4XTT9PTQMGX8W2U_BS8DG'
+fs_password = 'LU8klOK4RpefDGAz1ngl_w'
+
+FastSpringRequest.run(fs_username, fs_password)
